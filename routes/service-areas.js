2791cb2f80ed4d7190ed929ecc0b3ff857b3cfc8
userName <jre1622@gmail.com>
2025-03-12 16:49:47 +0700

    x

--- routes/service-areas.js
+++ routes/service-areas.js
@@ -3,22 +3,22 @@ const router = express.Router();
 
 // Inline service area data
 const serviceAreas = [
-  { name: "Minneapolis", slug: "minneapolis" },
-  { name: "St. Paul", slug: "st-paul" },
-  { name: "Bloomington", slug: "bloomington" },
-  { name: "Eden Prairie", slug: "eden-prairie" },
-  { name: "Plymouth", slug: "plymouth" },
-  { name: "Maple Grove", slug: "maple-grove" },
-  { name: "Coon Rapids", slug: "coon-rapids" },
-  { name: "Blaine", slug: "blaine" },
-  { name: "Anoka", slug: "anoka" },
-  { name: "Andover", slug: "andover" },
-  { name: "Brooklyn Park", slug: "brooklyn-park" },
-  { name: "Fridley", slug: "fridley" },
-  { name: "Champlin", slug: "champlin" },
-  { name: "Ramsey", slug: "ramsey" },
-  { name: "Shoreview", slug: "shoreview" },
-  { name: "Roseville", slug: "roseville" },
+  { name: "Minneapolis", slug: "minneapolis", population: 425115, median_home_value: 325000, median_home_age: 57 },
+  { name: "St. Paul", slug: "st-paul", population: 303820, median_home_value: 280000, median_home_age: 59 },
+  { name: "Bloomington", slug: "bloomington", population: 87398, median_home_value: 320000, median_home_age: 50 },
+  { name: "Eden Prairie", slug: "eden-prairie", population: 64870, median_home_value: 435000, median_home_age: 45 },
+  { name: "Plymouth", slug: "plymouth", population: 80666, median_home_value: 375000, median_home_age: 48 },
+  { name: "Maple Grove", slug: "maple-grove", population: 68228, median_home_value: 375000, median_home_age: 46 },
+  { name: "Coon Rapids", slug: "coon-rapids", population: 34456, median_home_value: 275000, median_home_age: 53 },
+  { name: "Blaine", slug: "blaine", population: 78268, median_home_value: 325000, median_home_age: 49 },
+  { name: "Anoka", slug: "anoka", population: 18336, median_home_value: 275000, median_home_age: 59 },
+  { name: "Andover", slug: "andover", population: 34256, median_home_value: 375000, median_home_age: 47 },
+  { name: "Brooklyn Park", slug: "brooklyn-park", population: 83346, median_home_value: 275000, median_home_age: 52 },
+  { name: "Fridley", slug: "fridley", population: 28888, median_home_value: 250000, median_home_age: 53 },
+  { name: "Champlin", slug: "champlin", population: 25442, median_home_value: 325000, median_home_age: 50 },
+  { name: "Ramsey", slug: "ramsey", population: 23685, median_home_value: 300000, median_home_age: 50 },
+  { name: "Shoreview", slug: "shoreview", population: 28734, median_home_value: 400000, median_home_age: 47 },
+  { name: "Roseville", slug: "roseville", population: 35836, median_home_value: 300000, median_home_age: 57 },
 ];
 
 // Base URL for canonical links (import from environment or config)
